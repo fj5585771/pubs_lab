50e70e1dfaa51eb11215414b8e78dcd8f497bc9b
Pedram Valiani <p.valiani@gmail.com>
2020-11-11 14:06:20 +0000

    tested drink has name -correct

--- tests/drink_test.py
+++ tests/drink_test.py
@@ -5,8 +5,12 @@ class TestDrink(unittest.TestCase):
     def setUp(self):
         self.drink = Drink("Bevvy", 5)
     
-    #test a drink object to check it has a name by referring to the price attribute of the drink object
+    #test a drink object to check it has a price by referring to the price attribute of the drink object
     
-    def test_has_drink(self):
+    def test_has_drink_price(self):
         self.assertEqual(self.drink.price, 5) 
+
+    #test a drink object to check it has a name by referring to the name attribute of the drink object
+    def test_has_drink_name(self):
+        self.assertEqual("Bevvy", self.drink.name) 
     

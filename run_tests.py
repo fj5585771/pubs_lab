af3735bcb3bf164766065fe27e8d3d828c0bf785
Pedram Valiani <p.valiani@gmail.com>
2020-11-11 12:25:47 +0000

    all files set up for coding

---
+++ run_tests.py
@@ -0,0 +1,7 @@
+import unittest
+from tests.customer_test import TestCustomer
+from tests.drink_test import TestDrink
+from tests.pub_tests import TestPub
+
+if __name__ == '__main__':
+    unittest.main()

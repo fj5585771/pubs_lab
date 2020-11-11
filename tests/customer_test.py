fbe34b2d1d925f6bf6bfcf944bd250a589cb6bcb
Pedram Valiani <p.valiani@gmail.com>
2020-11-11 14:12:12 +0000

    tested customer has a name correct

--- tests/customer_test.py
+++ tests/customer_test.py
@@ -4,4 +4,10 @@ from src.drink import Drink
 
 class TestCustomer(unittest.TestCase):
     def setUp(self):
+        self.customer = Customer("Fraser", 50)
+    
+    def test_customer_has_name(self):
+        self.assertEqual(self.customer.name, "Fraser")
+
+    def test_customer_has_wallet_value(self):
         pass
